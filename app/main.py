da0a1c2f339905a8ad6c05751d88e693426bb6b3
Mathieu Hentges <mathieuhentges@gmail.com>
2025-05-23 13:09:32 -0400

    Create form for application submission

--- app/main.py
+++ app/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, UploadFile, File, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI with Docker!"}
+
+@app.get("/", response_class=HTMLResponse)
+async def form_get(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
+
+
+@app.post("/submitApplication")
+async def handle_form(name: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
+    if file.content_type != "text/plain":
+        raise HTTPException(status_code=400, detail="Invalid file type. Only text file supported")
+
+    try:
+        content = await file.read()
+        text = content.decode("utf-8")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Failed to read the file")
+
+    return {
+        "name": name,
+        "description": description,
+        "filename": file.filename,
+        "file_content": text
+    }
